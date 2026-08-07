64579620a5faabb6345ba94d63ba859bba10c4b7
ram-creator-1156 <72293887+ram-creator-1156@users.noreply.github.com>
2026-08-08 02:00:11 +0530

    Delete routes directory

--- routes/showroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getShowDetails, getEpisodes } = require('../controllers/showController');
-
-router.get('/show-details', getShowDetails);
-router.get('/episodes', getEpisodes);
-
-module.exports = router;
