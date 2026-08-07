9d8373f6092de98eb1b623f619813844bf93fc93
ram-creator-1156 <72293887+ram-creator-1156@users.noreply.github.com>
2026-08-08 02:00:21 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const showRoutes = require('./routes/showRoutes');
-
-app.use('/', showRoutes);
-
-app.listen(8080, () => console.log('Server running on port 8080'));
